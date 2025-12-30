1be868a1c673e74bb5156b68e9d235043e204259
Phuc-Ming <152255899+Phuc-Ming@users.noreply.github.com>
2025-12-30 20:18:25 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
+<?php
+    //Kiểm tra
+    session_start();
 
+    if (isset($_SESSION['user_id'])) {
+        header("Location: cnotes/list.php");
+    } else {
+        header("Location: bauth/login.php");
+    }
+    exit();
+?>
