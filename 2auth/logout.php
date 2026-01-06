d5470c3133a585c5be64e681d5c70e456259a0d5
Phuc-Ming <152255899+Phuc-Ming@users.noreply.github.com>
2026-01-06 10:13:40 +0700

    Delete 2auth directory

--- 2auth/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();  //Bắt đầu session
-    session_destroy();  //Hủy session
-    header("Location: login.php");  //Đưa về trang đăng nhập
-    exit();  
-?>
--- 2auth/require_login.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (empty($_SESSION['user_id'])) {
-    header("Location: ../2auth/login.php");
-    exit();
-}
-?>
